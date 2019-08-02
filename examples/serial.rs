73f17eb5ced57e5391f1efd38d047788d03994a2
Vadim Kaushan <admin@disasm.info>
2019-08-02 15:48:56 +0300

    Update examples

--- examples/serial.rs
+++ examples/serial.rs
@@ -4,50 +4,52 @@
 
 extern crate panic_semihosting;
 
-use cortex_m::asm::delay;
 use cortex_m_rt::entry;
-use stm32_usbd::UsbBus;
-use stm32f1xx_hal::{prelude::*, stm32};
+use stm32f429_usbd_fs::{UsbBus, sprintln};
+use stm32f4xx_hal::{prelude::*, stm32};
 use usb_device::prelude::*;
+use embedded_hal::digital::v2::OutputPin;
 
-mod cdc_acm;
+static mut EP_MEMORY: [u32; 1024] = [0; 1024];
 
 #[entry]
 fn main() -> ! {
     let dp = stm32::Peripherals::take().unwrap();
 
-    let mut flash = dp.FLASH.constrain();
-    let mut rcc = dp.RCC.constrain();
+    let rcc = dp.RCC.constrain();
 
     let clocks = rcc
         .cfgr
         .use_hse(8.mhz())
         .sysclk(48.mhz())
         .pclk1(24.mhz())
-        .freeze(&mut flash.acr);
+        .require_pll48clk()
+        .freeze();
 
-    assert!(clocks.usbclk_valid());
+    let gpiod = dp.GPIOD.split();
+    stm32f429_usbd_fs::debug::configure(dp.USART3, gpiod.pd8, gpiod.pd9, 115_200.bps(), clocks);
 
-    let mut gpioa = dp.GPIOA.split(&mut rcc.apb2);
+    sprintln!("==========================");
 
-    // BluePill board has a pull-up resistor on the D+ line.
-    // Pull the D+ pin down to send a RESET condition to the USB bus.
-    let mut usb_dp = gpioa.pa12.into_push_pull_output(&mut gpioa.crh);
-    usb_dp.set_low();
-    delay(clocks.sysclk().0 / 100);
+    let gpiob = dp.GPIOB.split();
+    let mut led = gpiob.pb7.into_push_pull_output();
+    led.set_low(); // Turn off
 
-    let usb_dm = gpioa.pa11;
-    let usb_dp = usb_dp.into_floating_input(&mut gpioa.crh);
 
-    let usb_bus = UsbBus::new(dp.USB, (usb_dm, usb_dp));
+    let gpioa = dp.GPIOA.split();
 
-    let mut serial = cdc_acm::SerialPort::new(&usb_bus);
+    let usb_dm = gpioa.pa11.into_alternate_af10();
+    let usb_dp = gpioa.pa12.into_alternate_af10();
+
+    let usb_bus = UsbBus::new((dp.OTG_FS_GLOBAL, dp.OTG_FS_DEVICE, dp.OTG_FS_PWRCLK), (usb_dm, usb_dp), unsafe { &mut EP_MEMORY });
+
+    let mut serial = usbd_serial::SerialPort::new(&usb_bus);
 
     let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x5824, 0x27dd))
         .manufacturer("Fake company")
         .product("Serial port")
         .serial_number("TEST")
-        .device_class(cdc_acm::USB_CLASS_CDC)
+        .device_class(usbd_serial::USB_CLASS_CDC)
         .build();
 
     loop {
@@ -59,6 +61,8 @@ fn main() -> ! {
 
         match serial.read(&mut buf) {
             Ok(count) if count > 0 => {
+                led.set_high(); // Turn on
+
                 // Echo back in upper case
                 for c in buf[0..count].iter_mut() {
                     if 0x61 <= *c && *c <= 0x7a {
@@ -66,9 +70,19 @@ fn main() -> ! {
                     }
                 }
 
-                serial.write(&buf[0..count]).ok();
+                let mut write_offset = 0;
+                while write_offset < count {
+                    match serial.write(&buf[write_offset..count]) {
+                        Ok(len) if len > 0 => {
+                            write_offset += len;
+                        },
+                        _ => {},
+                    }
+                }
             }
             _ => {}
         }
+
+        led.set_low(); // Turn off
     }
 }
--- examples/test_class.rs
+++ examples/test_class.rs
@@ -3,41 +3,34 @@
 
 extern crate panic_semihosting;
 
-use cortex_m::asm::delay;
 use cortex_m_rt::entry;
-use stm32f1xx_hal::{prelude::*, stm32};
+use stm32f4xx_hal::{prelude::*, stm32};
 
-use stm32_usbd::UsbBus;
+use stm32f429_usbd_fs::UsbBus;
 use usb_device::test_class::TestClass;
+
+static mut EP_MEMORY: [u32; 1024] = [0; 1024];
 
 #[entry]
 fn main() -> ! {
     let dp = stm32::Peripherals::take().unwrap();
 
-    let mut flash = dp.FLASH.constrain();
-    let mut rcc = dp.RCC.constrain();
+    let rcc = dp.RCC.constrain();
 
     let clocks = rcc
         .cfgr
         .use_hse(8.mhz())
         .sysclk(48.mhz())
         .pclk1(24.mhz())
-        .freeze(&mut flash.acr);
+        .require_pll48clk()
+        .freeze();
 
-    assert!(clocks.usbclk_valid(), "usb clocks not valid");
+    let gpioa = dp.GPIOA.split();
 
-    let mut gpioa = dp.GPIOA.split(&mut rcc.apb2);
+    let usb_dm = gpioa.pa11.into_alternate_af10();
+    let usb_dp = gpioa.pa12.into_alternate_af10();
 
-    // BluePill board has a pull-up resistor on the D+ line.
-    // Pull the D+ pin down to send a RESET condition to the USB bus.
-    let mut usb_dp = gpioa.pa12.into_push_pull_output(&mut gpioa.crh);
-    usb_dp.set_low();
-    delay(clocks.sysclk().0 / 100);
-
-    let usb_dm = gpioa.pa11;
-    let usb_dp = usb_dp.into_floating_input(&mut gpioa.crh);
-
-    let usb_bus = UsbBus::new(dp.USB, (usb_dm, usb_dp));
+    let usb_bus = UsbBus::new((dp.OTG_FS_GLOBAL, dp.OTG_FS_DEVICE, dp.OTG_FS_PWRCLK), (usb_dm, usb_dp), unsafe { &mut EP_MEMORY });
 
     let mut test = TestClass::new(&usb_bus);
 

4cffbe5a2d4d25fa308dd0adcca7aa5a42776548
Vadim Kaushan <admin@disasm.info>
2019-12-22 12:41:43 +0300

    Update CI rules

--- ci/script.sh
+++ ci/script.sh
@@ -2,11 +2,6 @@
 
 set -euxo pipefail
 
-cargo check --no-default-features --features stm32f042xx
-#cargo check --no-default-features --features stm32f048xx
-#cargo check --no-default-features --features stm32f072xx
-#cargo check --no-default-features --features stm32f078xx
-cargo check --no-default-features --features stm32f103xx
-cargo check --no-default-features --features stm32f303xc
-cargo check --no-default-features --features stm32l4x2xx
-cargo check --no-default-features --features stm32f103xx --target thumbv7m-none-eabi --examples
+cargo check --features "stm32f429xx fs"
+cargo check --features "stm32f429xx hs"
+cargo check --features "stm32f401xx"

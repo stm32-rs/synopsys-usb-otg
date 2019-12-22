22e89f468341d6a2af8370119a8d243756b410ea
Vadim Kaushan <admin@disasm.info>
2019-12-22 12:07:10 +0300

    Introduce UsbPeripheral trait

--- src/lib.rs
+++ src/lib.rs
@@ -24,3 +24,20 @@ pub use crate::target::usb_pins::UsbPinsType;
 pub type UsbBusType = UsbBus<UsbPinsType>;
 
 mod ral;
+
+/// A trait for device-specific USB peripherals. Implement this to add support for a new hardware
+/// platform. Peripherals that have this trait must have the same register block as STM32 USB OTG
+/// peripherals.
+pub unsafe trait UsbPeripheral: Send + Sync {
+    /// Pointer to the register block
+    const REGISTERS: *const ();
+
+    /// true for High Speed variants of the peripheral, false for Full Speed
+    const HIGH_SPEED: bool;
+
+    /// FIFO size in 32-bit words
+    const FIFO_DEPTH_WORDS: usize;
+
+    /// Enables USB device on its peripheral bus
+    fn enable();
+}
